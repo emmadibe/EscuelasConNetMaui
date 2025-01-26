2ce93775bd65318724d3c78990079aca0c3a6cd5
emmadibe <127629968+emmadibe@users.noreply.github.com>
2025-01-26 14:30:57 -0300

    jkjk

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EscuelaConMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EscuelaConMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
